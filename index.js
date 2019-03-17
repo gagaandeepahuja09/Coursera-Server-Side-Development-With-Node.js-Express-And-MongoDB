5c1184254182b30fa671a5df9b029daf2748d8ef
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-03-17 21:07:15 +0530

    simple node example

---
+++ index.js
@@ -0,0 +1,19 @@
+let rect = {
+	area: (x, y) => x*y, 
+	perimeter: (x, y) => 2*(x+y)
+};
+
+solveRect = (l, b) => {
+	if(l <= 0 || b <= 0) {
+		console.log("Dimensions of rectangle should be positive");
+	}
+	else {
+		console.log("Area of rectangle is : " + rect.area(l, b));
+		console.log("Perimeter of rectangle is : " + rect.perimeter(l, b));
+	}
+} 
+
+solveRect(2, 5);
+solveRect(3, 4);
+solveRect(0, 5);
+solveRect(2, -5);
