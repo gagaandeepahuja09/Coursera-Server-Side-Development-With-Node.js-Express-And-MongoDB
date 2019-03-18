17a2e7dfbca25a9b9157230446bb4d94f7b5933d
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-03-18 09:24:58 +0530

    understanding callbacks and error handling

--- index.js
+++ index.js
@@ -5,14 +5,20 @@
 let rect = require('./rectangle');
 
 solveRect = (l, b) => {
-	if(l <= 0 || b <= 0) {
-		console.log("Dimensions of rectangle should be positive");
-	}
-	else {
-		console.log("Area of rectangle is : " + rect.area(l, b));
-		console.log("Perimeter of rectangle is : " + rect.perimeter(l, b));
-	}
-} 
+	console.log("Solving for l = " + l + " and b = " + b);
+	console.log(`This statement is after the call to rect() but won't wait for the rect 
+		function to be executed and it will carry on doing something else`);
+	rect(l, b, (err, rectangle) => {
+		if(err) {
+			console.log("ERROR: ", err.message);
+		}
+		else {
+			console.log("For dimensions " + l + " and " + b
+				+ " Area of rectangle is " + rectangle.area(l, b) 
+				+ " Perimeter of rectangle is " + rectangle.perimeter(l, b));
+		}
+	});
+}
 
 solveRect(2, 5);
 solveRect(3, 4);
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,17 @@
-exports.perimeter = (x, y) => (2 * (x + y));
-exports.area = (x, y) => (x * y);
+// Callback: Async equivalent of a function
+module.exports = (x, y, callback) => {
+	if(x <= 0 || y <= 0) {
+		setTimeout(() =>
+			callback(new Error("Rectangle Dimensions should be positive l = " + x + " b = " + y), 
+			null),
+			2000);
+	}
+	else {
+		setTimeout(() => callback(null, 
+			{
+				perimeter: (x, y) => (2*(x + y)),
+				area: (x, y) => (x*y),
+			}),
+		2000);
+	}
+}
